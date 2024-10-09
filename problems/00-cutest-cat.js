6bfd129a7c51350d7c5d88062d8d535d5ba338ed
rmdelapaz <raydelapaz@gmailcom>
2024-10-09 09:18:18 -0700

    testing

--- problems/00-cutest-cat.js
+++ problems/00-cutest-cat.js
@@ -1,9 +1,10 @@
 /*
 
 Fix the `cutestCat` function. Should return the cat with the highest `cuteness`
-rating.
+rating. Whee
 */
-
+// testting
+//more changes
 function cutestCat(cats) {
   let cutest;
   let i = 0;
@@ -20,10 +21,10 @@ function cutestCat(cats) {
 }
 
 const cats = [
-  { name: 'Fluffy', cuteness: 9 },
-  { name: 'Princess', cuteness: 6 },
-  { name: 'Tiger', cuteness: 7 },
-  { name: 'Indie', cuteness: 5 },
-]
+  { name: "Fluffy", cuteness: 9 },
+  { name: "Princess", cuteness: 6 },
+  { name: "Tiger", cuteness: 7 },
+  { name: "Indie", cuteness: 5 },
+];
 //debugger
 console.log(cutestCat(cats)); // { name: 'Fluffy', cuteness: 9 }
